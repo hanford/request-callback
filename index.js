bedafcce2ba930d81e6e0d5ec3874ef33d71a0f4
hanford <jackhanford@gmail.com>
2017-09-18 18:22:11 -0700

    assert callback is defined

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const window = require('global/window')
+const assert = require('assert')
 
 module.exports = function requestCallback (callback) {
+  assert(callback, 'callback must be defined')
+
   if (window.requestIdleCallback) {
     return window.requestIdleCallback(callback)
   } else {
